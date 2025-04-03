ec1fb4cc7b868bca2494e29923f4a43d47174122
maithamphysics <57330201+maithamphysics@users.noreply.github.com>
2025-04-03 21:09:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 # ---- Configuration ----
 st.set_page_config(
-    page_title="Kut Water AI NOWW", 
+    page_title="Kut Water AI CHANGEEE", 
     page_icon="💧",
     layout="centered"
 )
